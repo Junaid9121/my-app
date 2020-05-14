18f166054f5df78cc64c7e2de9ea419867ba7cd4
JUNAID SHEIKH <sheikhusmanstar@gmail.com>
2020-05-14 22:09:55 +0500

    Lab Task

---
+++ app/src/main/java/com/ekorydes/bscs6tha310320/MainContentPage.java
@@ -0,0 +1,14 @@
+package com.ekorydes.bscs6tha310320;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainContentPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_content_page);
+    }
+}
